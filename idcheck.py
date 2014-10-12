5145099f1c68eaf8fe01da69cbf6254819fdf436
Wang Zhicheng <wangzhicheng.thu@gmail.com>
2014-10-12 08:51:18 +0800

    ex 6-2: string identifiers: idcheck.py version 2.0

--- idcheck.py
+++ idcheck.py
@@ -4,23 +4,31 @@ check for valid Python identifiers
 """
 
 from string import  ascii_letters as _ascii_letters, digits as _digits
+from keyword import kwlist as _kwlist
 
 _alphas = _ascii_letters + '_'
 _nums = _digits
+_alphnums = _alphas + _nums
 
-print "welcome to the identifier checker v1.0"
-print "testees must be at least 2 chars long."
-ainput = raw_input("Indentifier to test? ")
+print "welcome to the identifier checker v2.0"
+#print "testees must be at least 2 chars long."
+ainput = raw_input("Indentifier to test? ").strip()
 
 if len(ainput) > 1:
     if ainput[0] not in _alphas:
         print "Invalid: first symbol must be alphabetic."
     else:
-        _alphnums = _alphas + _nums
         for otherchar in ainput[1:]:
             if otherchar not in _alphnums:
                 print "invalid: remaining symbol must be alphanumeric."
                 break
         else:
-            print "okay as an identifier."
-
+            if ainput in _kwlist:
+                print "You just entered a keyword: %s" % ainput
+            else:
+                print "okay as an identifier."
+else:
+    if ainput in _ascii_letters:
+        print "Okay as an identifier"
+    else:
+        print "invalid: single char identifier must be an ascii letter."
