5ff05c93f6f3acfa84dc59fe4fbb1a2c75d32473
Wang Zhicheng <wzc_jd01@126.com>
2014-10-16 13:27:10 +0800

    conditional: max factor

---
+++ maxFact.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#__author__ = 'Wang Zhicheng'
+
+from __future__ import print_function
+
+
+def showMaxFactor(num):
+    count = num / 2
+    while count > 1:
+        if num % count == 0:
+            print('largest factor of {0:d} is {1:d}'.format(num, count))
+            break
+        count -= 1
+    else:
+        print('{0:d} is prime'.format(num))
+
+
+for eachNum in range(10, 21):
+    showMaxFactor(eachNum)
